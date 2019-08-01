6298a6e24e084dd920b9e37929d78842c52a8ee2
TIAN MBPR <tianyow@TIAN-MBPR.local>
2019-08-01 10:22:36 +0800

    Config 頁面新增 API Key 欄位

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # 交談式網銀
 
+2019-0801 Config頁面新增 API Key欄位.
+
 2019-0725 First commit.
--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,12 @@
 $(function () {
-  $('[data-toggle="tooltip"]').tooltip()
+
+	// Tooltip
+  $('[data-toggle="tooltip"]').tooltip();
+
+  // File Upload label
+  $('input[type="file"]').change(function(e){
+  	var fileName = e.target.files[0].name;
+  	$('.custom-file-label').html(fileName);
+  });
+  
 })
